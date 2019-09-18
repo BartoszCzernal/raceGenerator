58bfd8e96d3fefd348405e08779e5209a06b18a4
BartoszCzernal <bartekczernal@gmail.com>
2019-09-18 15:11:37 +0200

    first commit

---
+++ RaceGenerator/src/main/java/com/kart/RaceGenerator/RaceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.kart.RaceGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceGeneratorApplication.class, args);
+	}
+
+}
